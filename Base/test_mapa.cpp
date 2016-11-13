52ace146b2a9e787509f8f25b6b30caae395d98c
jonseijo <jon.seijo@gmail.com>
2016-11-13 19:19:06 -0300

    Agrega interfax de test de mapa

--- Base/test_mapa.cpp
+++ Base/test_mapa.cpp
@@ -8,17 +8,33 @@
 using namespace aed2;
 
 void test_vacio() {
-  ASSERT( false );
+  	Mapa map;
+  	ASSERT(map.Tam() == 0);
+  	// tambien chequear que el conjunto de coordenadas es vacio
 }
 
 void test_agregar_coordenada(){
 	ASSERT( false );
 }
 
+void test_posExistente() {
+	ASSERT( false );
+}
+
+void test_hayCamino() {
+	ASSERT( false );
+}
+
+void test_tam() {
+	ASSERT( false );
+}
 
 int main(int argc, char **argv){
   RUN_TEST(test_vacio);
   RUN_TEST(test_agregar_coordenada);
+  RUN_TEST(test_posExistente);
+  RUN_TEST(test_hayCamino);
+  RUN_TEST(test_tam);
 
   return 0;
 }
