2d0f9daa2de7919c0f817fcc023530f6ca20d505
Jonathan Seijo <jonathan.seijo@avature.net>
2016-11-18 11:20:03 -0300

    Agrega test de definir

--- Base/test_arreglo.cpp
+++ Base/test_arreglo.cpp
@@ -35,8 +35,44 @@ void test_constructor() {
     }
 }
 
+// Arreglo.Definir(pos, valor)
+void test_definir() {
+    // Arreglo comun
+    Arreglo<Nat> a(10);
+    a.Definir(0, 99);
+    a.Definir(1, 99);
+    ASSERT(a.Definido(0));
+    ASSERT(a.Definido(1));
+    ASSERT(a.Tamanho() == 10);
+
+    // Arreglo de punteros
+
+    // // ASI SON LEAKS
+    // // HAY QUE HACER DELETE A MANOPLA
+    // Arreglo<Nat*> b(10);
+    // b.Definir(0, new Nat(9999));
+    // b.Definir(1, new Nat(9999));
+    // ASSERT(b.Definido(0));
+    // ASSERT(b.Definido(1));
+    // ASSERT(b.Tamanho() == 10);
+
+    // Haciendo deletes a mano no hay leaks
+    Arreglo<Nat*> c(10);
+    Nat* test1 = new Nat(999);
+    Nat* test2 = new Nat(999);
+    c.Definir(0, test1);
+    c.Definir(1, test2);
+    ASSERT(c.Definido(0));
+    ASSERT(c.Definido(1));
+    ASSERT(c.Tamanho() == 10);
+
+    delete test1;
+    delete test2;
+}
+
 int main(int argc, char **argv){
     RUN_TEST(test_vacia);
     RUN_TEST(test_constructor);
+    RUN_TEST(test_definir);
     return 0;
 }
